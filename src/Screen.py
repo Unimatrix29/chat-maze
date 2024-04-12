e8babd2d3eea57da060bd35d8601b8ad5206cf31
Louis <s0591400@htw-berlin.de>
2024-04-12 14:37:39 +0200

    screen setup

--- src/Screen.py
+++ src/Screen.py
@@ -1,8 +1,11 @@
 import pygame
 
+#setup pygame window
 pygame.init()
-
 win = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("chat_leap")
 
-
-
+#16x16 display
+screen_width = 16
+screen_height = 16
+screen = [[0 for x in range(screen_width)] for y in range(screen_height)] 
