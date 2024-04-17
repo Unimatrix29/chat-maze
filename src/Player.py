8048e5facc9fce14dce17333ccc2eab87109ea6b
BuildTools <oskar.fulde@student.htw-berlin.de>
2024-04-17 20:58:58 +0200

    added Screen obj to constructor

--- src/Player.py
+++ src/Player.py
@@ -1,4 +1,11 @@
+import Screen 
+
 class Palyer:
-    def __init__(self, maze) -> None:
+    def __init__(self, maze):
         self.activeMaze = maze
+
+        #dependent on the actual implementation of the maze 
         self.currentPosition = maze[2]
+
+        #added a variable to hold and initiate a screen obj 
+        self.__screen = Screen()
