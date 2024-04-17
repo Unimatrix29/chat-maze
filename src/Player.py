7cbfc1c6ecf6cd2ed2c78ef496e4c159f882b0d2
BuildTools <oskar.fulde@student.htw-berlin.de>
2024-04-17 21:35:18 +0200

    fixed error

--- src/Player.py
+++ src/Player.py
@@ -5,7 +5,7 @@ class Palyer:
         self.activeMaze = maze
 
         #dependent on the actual implementation of the maze 
-        self.currentPosition = maze[2]
+        self.currentPosition = maze[1]
 
         #added a variable to hold and initiate a screen obj 
         self.__screen = Screen()
