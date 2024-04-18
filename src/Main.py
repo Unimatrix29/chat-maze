1f6d3410443edf43f57ef47391305662d34a6105
Louis <s0591400@htw-berlin.de>
2024-04-18 12:59:28 +0200

    installed keyboard package & Main class setup

--- src/Main.py
+++ src/Main.py
@@ -1,5 +1,19 @@
 import random
+import keyboard
 import Screen
 import MazeGenerator
 import Player
+
+running = False
+
+#program start
+running = True
+Screen.setup_screen()
+while running:
+    if keyboard.is_pressed('q'):  
+            print('You Pressed A Key!')
+            break  
+    
+
+
 
--- src/Screen.py
+++ src/Screen.py
@@ -1,15 +1,14 @@
 import pygame
 import random
-import MazeGenerator
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+RED = (127, 127, 127)
 
 SCREEN_SIZE = (16, 16)
 GRID_SIZE = 16
 CELL_SIZE = 30
 PIXEL_SIZE = 24
-
 
 def setup_screen():
     global screen
@@ -17,12 +16,14 @@ def setup_screen():
     screen = pygame.display.set_mode([480, 480])
     pygame.display.set_caption("Chat_Leap")
 
-def update_screen(maze = MazeGenerator.get_preset(), player = [0, 0]):
+def update_screen(maze, player_pos):
     screen.fill(BLACK)
     for y in range(GRID_SIZE):
         for x in range(GRID_SIZE):
             if maze[y][x] == 1:
                 pygame.draw.rect(screen, WHITE, (x * CELL_SIZE, y * CELL_SIZE, PIXEL_SIZE, PIXEL_SIZE))
+            if player_pos == [x, y]:
+                pygame.draw.rect(screen, RED, (x * CELL_SIZE, y * CELL_SIZE, PIXEL_SIZE, PIXEL_SIZE))
 
     pygame.display.flip()
 
