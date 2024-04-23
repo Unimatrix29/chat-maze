4e6af266d36c3d087ab46196219a53572a0c354e
Robert Koch <robert.koch@student.htw-berlin.de>
2024-04-23 19:52:05 +0200

    Init state

--- src/Main.py
+++ src/Main.py
@@ -1,26 +1,11 @@
-import random
-import keyboard
-from Screen import Screen 
+from Screen import Screen
 from MazeGenerator import MazeGenerator
-import Player
 
-running = False
+window = Screen()
+mazeGenerator = MazeGenerator()
 
-main_screen = Screen()
-maze_generator = MazeGenerator()
-#program start
-running = True
+window.setup_screen()
+maze = mazeGenerator.get_preset("maze_1")
 
-main_screen.setup_screen()
-main_screen.update_screen(maze_generator.random_maze(), [5, 7])
-#while running:
 while True:
-    pass
-
-    
-    
-    
-
-
-
-
+    window.update_screen(maze, [4, 2])
