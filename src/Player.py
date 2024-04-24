8db0b024f957f22ef9d21b306b5af57f420ebbdc
Oskar <oskar.fulde@student.htw-berlin.de>
2024-04-24 23:17:19 +0200

    removed some usless commentary

--- src/Player.py
+++ src/Player.py
@@ -2,14 +2,11 @@ from Screen import Screen
 
 class Player:
     def __init__(self, maze, screen):
-        #assuming that acivteMaze is a 3D array and the actual representation of the maze is at index 0, 
-        #the start position at index 1 and the end position at index 2
         self.activeMaze = maze
 
-        #dependent on the actual implementation of the maze 
         self.currentPosition = maze[1]
-
-        #added a variable to hold and initiate a screen obj 
+        
+        #not sure if needed
         self.__screen = screen
 
     def UpdateMaze(self, newMaze):
