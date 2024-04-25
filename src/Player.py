5ecb39d20390a92ce181fcca10f3bdc00db4fc7e
Oskar Fulde <oskar.fulde@student.htw-berlin.de>
2024-04-25 14:18:36 +0000

    check_wall logic added

--- src/Player.py
+++ src/Player.py
@@ -6,7 +6,6 @@ class Player:
 
         self.currentPosition = maze[1]
         
-        #not sure if needed
         self.__screen = screen
 
     def UpdateMaze(self, newMaze):
@@ -16,25 +15,26 @@ class Player:
         self.newPosition = self.currentPosition
         self.newPosition[1] = self.currentPosition[1] - 1
 
-        #not sure if needed 
-        #if not self.__screen.CheckWall():
-        self.currentPosition = self.newPosition
+        if not self.__screen.check_wall(self.activeMaze, self.newPosition):
+            self.currentPosition = self.newPosition
 
     def MoveDown(self):
         self.newPosition = self.currentPosition
         self.newPosition[1] = self.currentPosition[1] + 1
 
-        #if not self.__screen.CheckWall():
-        self.currentPosition = self.newPosition
+        if not self.__screen.check_wall(self.activeMaze, self.newPosition):
+            self.currentPosition = self.newPosition
 
     def MoveLeft(self):
         self.newPosition = self.currentPosition 
         self.newPosition[0] = self.currentPosition[0] - 1
-        if not self.__screen.CheckWall(self.newPosition):
+
+        if not self.__screen.check_wall(self.activeMaze, self.newPosition):
             self.currentPosition = self.newPosition
 
     def MoveRight(self):
         self.newPosition = self.currentPosition
         self.newPosition[0] = self.currentPosition[0] + 1
-        if not self.__screen.CheckWall(self.newPosition):
+
+        if not self.__screen.check_wall(self.activeMaze, self.newPosition):
             self.currentPosition = self.newPosition
