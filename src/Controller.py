0f1639768d1d6f15748456f23bf647ce0dc818f2
Oskar <oskar.fulde@student.htw-berlin.de>
2024-04-30 14:07:13 +0200

    small changes to make it readable

--- src/Controller.py
+++ src/Controller.py
@@ -1,4 +1,3 @@
-import random 
 from openai import OpenAI
 
 GPT_MODEL = "gpt-3.5-turbo"
@@ -8,14 +7,12 @@ SYSTEM_PROMPT =  {"content": "You ar a helpful mathe teacher.", "role": "system"
 class Controller():
 
     def __init__(self):
-        self.userPrompt = {"content": "", "role": "user"}
+        self.userPrompt = {"content": "Test", "role": "user"}
         self.userHistory = []
 
         self.move_options = {"up": [0, -1], "down": [0, 1], "left": [-1, 0], "right": [1, 0], "deny": [0, 0]}
 
         self.client = OpenAI(api_key=API_KEY)
-        self.client.api_key
-        
 
     def gpt_call(self):
         
@@ -39,15 +36,13 @@ class Controller():
         response_massage = response.choices[0].message
 
         #safe user history
-        self.userHistory.append(response_massage)
-        #not in here
-        self.userHistory.append(self.userPrompt)
+        self.userHistory.extend(response_massage, self.userPrompt)
 
         #test
         print(response_massage.content)
 
         
-        return response_massage.content
+        return response_massage.content.lower().strip()
 
 
  
