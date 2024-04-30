7064c334a0af9512e5c2d27e4d10219b564a90c8
Oskar <oskar.fulde@student.htw-berlin.de>
2024-04-30 13:20:10 +0200

    made the code more readable

--- src/Controller.py
+++ src/Controller.py
@@ -9,10 +9,12 @@ class Controller():
 
     def __init__(self):
         self.userPrompt = ""
+        self.userHistory = []
+        self.move_options = {"up": [0, -1], "down": [0, 1], "left": [-1, 0], "right": [1, 0], "deny": [0, 0]}
 
         self.client = OpenAI(api_key=API_KEY)
         self.client.api_key
-        self.move_options = {"up": [0, -1], "down": [0, 1], "left": [-1, 0], "right": [1, 0], "deny": [0, 0]}
+        
 
     def __gpt_call(self):
         
@@ -23,11 +25,9 @@ class Controller():
 
         response = self.client.chat.completions.create(
         model=GPT_MODEL,
-        messages=[
-                {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-                {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-            ]
+        messages=massage
         )
+        
         response_content = response.choices[0].message.content
 
         print(response_content)
