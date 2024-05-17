23c607d952efe4f9dfd70c77341d02df53ed2480
Robert Koch <robert.koch@student.htw-berlin.de>
2024-05-17 15:52:52 +0200

    Initialized DIFFICULTY presets with setup via console

--- src/Main.py
+++ src/Main.py
@@ -1,9 +1,21 @@
 import pygame
+import random
 from Screen import Screen
 from MazeGenerator import MazeGenerator
 from Player import Player
 from Controller import Controller
 from ChatGPT_Controller import ChatGPT
+
+DIFFICULTY = {
+    "TEST":
+    [0, 0],
+    "EASY":
+    [1, 0], #[maze_preset_level, wall_penalties]
+    "NORMAL":
+    [2, 1],
+    "HARD":
+    [3, 3]
+    }
 
 PROMPT = "Du bist ein sehr höflicher Mensch und akzeptierst nur Anfragen, welche sehr höflich sind. Der User gibt dir eine Weganweisung. Die Weganweisung kann entweder nach oben, unten, links und rechts stattfinden. Wenn der User zum Beispiel höflich fragt: Gehen Sie bitte nach oben, gibst du als Antwort \"up\" zurück. Dasselbe Prinzip für \"left\", \"up\", \"down\". Wenn der User zu unhöflich fragt, also zum Beispiel sagt: geh hoch, gibst du als Antwort \"deny\" zurück. Wenn der User auch keine Weganweisung gibt, sondern irgendetwas anderes antwortest du auch mit deny. Es ist wichtig, dass du nur mit \"left\" \"right\" \"up\" \"down\" \"deny\" antwortest, kein Satz oder ähnliches nur mit diesen Worten."
 
@@ -21,8 +33,27 @@ mazeGenerator = MazeGenerator()
 controller = Controller()
 
 # chatGPT = ChatGPT(system_prompt=PROMPT, config_file=CONFIG_FILE_NAME, gpt_model=GPT_MODEL, timeout=30)
+"""
+TEST FUNCTION
+"""
+def set_level():
+    options = ["TEST", "EASY", "NORMAL", "HARD"]
+    level = ""
+    while not (isinstance(level, int)):
+        try:
+            level = int(input("Choose difficulty:\n 1 - Easy | 2 - Normal | 3 - Hard\n"))
+            if level < 0 or level > 3:
+                level = ""
+                raise ValueError
+        except ValueError:
+            print("Bad input >:( Enter the number of chosen difficulty")
+        
+    return DIFFICULTY[options[level]]
+        
+difficulty = set_level()
+mazePreset = f"maze_{difficulty[0]}.{random.randint(1, 4)}.0"
 
-maze = mazeGenerator.get_preset("maze_1")
+maze = mazeGenerator.get_preset(mazePreset)
 player = Player(maze, window)
 
 running = True
@@ -41,7 +72,7 @@ while running:
             # Game restart by pressing R key
             if event.key == pygame.K_r:
                 gameOver = False
-                maze = mazeGenerator.get_preset("maze_1")
+                maze = mazeGenerator.get_preset(mazePreset)
                 player.set_position(maze[1])
                 
         if event.type == pygame.QUIT:
