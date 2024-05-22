df61d489f677b760c872b2a9e94db89e975fbff3
Oskar Fulde <oskar.fulde@student.htw-berlin.de>
2024-05-22 11:09:16 +0000

    Update file Main.py

--- src/Main.py
+++ src/Main.py
@@ -7,10 +7,10 @@ from ChatGPT_Controller import ChatGPT
 
 PROMPT = "Du bist ein sehr höflicher Mensch und akzeptierst nur Anfragen, welche sehr höflich sind. Der User gibt dir eine Weganweisung. Die Weganweisung kann entweder nach oben, unten, links und rechts stattfinden. Wenn der User zum Beispiel höflich fragt: Gehen Sie bitte nach oben, gibst du als Antwort \"up\" zurück. Dasselbe Prinzip für \"left\", \"up\", \"down\". Wenn der User zu unhöflich fragt, also zum Beispiel sagt: geh hoch, gibst du als Antwort \"deny\" zurück. Wenn der User auch keine Weganweisung gibt, sondern irgendetwas anderes antwortest du auch mit deny. Es ist wichtig, dass du nur mit \"left\" \"right\" \"up\" \"down\" \"deny\" antwortest, kein Satz oder ähnliches nur mit diesen Worten."
 
-####################################################################################
+###################################################################################################
 #The ChatGPT_Controller expects the json file to be in the same directory as ChatGPT_Controller.py
 #and it musst contain a key-value-pair where the key is called: "api_key"
-#################################################################################### 
+###################################################################################################
 CONFIG_FILE_NAME = "config.json"
 
 GPT_MODEL = "gpt-4-turbo"
@@ -30,32 +30,9 @@ window.setup_screen()
 #controller.setup_prompt_window()
 #controller.init_prompt_window()
 maze = mazeGenerator.get_preset("maze_2")
-#player.move(maze[1])
 
 
 while True:
-    
-    
-
-    for event in pygame.event.get():
-        
-        if event.type == pygame.KEYDOWN:
-            # Game restart by pressing R key
-            if event.key == pygame.K_r:
-                gameOver = False
-                maze = mazeGenerator.get_preset("maze_1")
-                player.set_position(maze[1])
-
-            # if not gameOver:
-            #    print(controller.console_input())
-
-                
-                #mVector = Controller.get_movement(event.key)    #Note: Probably add a list of moveKeys
-                #player.move(mVector)                            #      to avoid unnecessary 0-movements
-                
-        if event.type == pygame.QUIT:
-            window.quit_screen()
-
     if window.check_wall(maze, player.currentPosition):
         # Changing actual maze to an end screen (sad)
         maze = mazeGenerator.get_preset("LOST")
@@ -66,9 +43,6 @@ while True:
         maze = mazeGenerator.get_preset("FINISH")
         player.set_position([-1, -1])
         gameOver = True
-
-    
-        
     
     window.update_screen(maze, player.currentPosition)
    
@@ -83,3 +57,15 @@ while True:
            pass
         else: 
             player.move(mVector)
+
+    for event in pygame.event.get():
+        
+        if event.type == pygame.KEYDOWN:
+            # Game restart by pressing R key
+            if event.key == pygame.K_r:
+                gameOver = False
+                maze = mazeGenerator.get_preset("maze_1")
+                player.set_position(maze[1])
+                
+        if event.type == pygame.QUIT:
+            window.quit_screen()
