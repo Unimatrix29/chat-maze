2c5787235cb6eee5a03933b0dea8ca8532ed88f6
Oskar <oskar.fulde@student.htw-berlin.de>
2024-05-27 00:47:35 +0200

    updeted chatgpt_movment to use the new ChatGPT_Controller

--- src/ChatGPT_Movment_Controller.py
+++ src/ChatGPT_Movment_Controller.py
@@ -1,19 +1,22 @@
 import openai
 from openai import OpenAI
-from ChatGPT_Controller import chatgpt_text
+from ChatGPT_Controller import ChatGPT
 
 class chatgpt_movment():
     
-    def __init__(self, client, system_prompt="", history=None, gpt_model="gpt-3.5-turbo"):
-        self.history = history
-        self.textChatGPT = chatgpt_text(client, system_prompt, gpt_model)
+    def __init__(self, chatgpt, system_prompt):
+        self.chatgpt = chatgpt
+        self.sysPrompt = system_prompt
         self.move_options = {"up": [0, -1], "down": [0, 1], "left": [-1, 0], "right": [1, 0], "deny": [0, 0]}
         
         
-    def get_movement_vector(self, userInput, temperature=None):
+    def get_vector(self, userInput, temperature):
+        
+        message = ChatGPT.construct_message(userInput, self.sysPrompt)
+        
         #api response 
         try:
-            chat_response = self.textChatGPT.chat_completion_request(userInput, history=self.history, temperature=temperature)
+            chat_response = self.chatgpt.text_to_text(message, temperature)
         except Exception as e: 
             return e 
         
