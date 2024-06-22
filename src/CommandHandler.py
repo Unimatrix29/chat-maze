0ee995ead5db1ae1af0e5eb9b40e08694428ab57
Robert Koch <robert.koch@student.htw-berlin.de>
2024-06-22 14:07:43 +0200

    Changed help message colour from System to GPT-Role

--- src/CommandHandler.py
+++ src/CommandHandler.py
@@ -47,7 +47,7 @@ class Command():
     
     def __help(self):
         promptKey = self.game.prompt[0]
-        self.game.screen.add_chat_text(self.help_txt.get(promptKey), "System")
+        self.game.screen.add_chat_text(self.help_txt.get(promptKey), promptKey)
     
     def __info(self):
         self.game.screen.add_chat_text(self.info_txt)
